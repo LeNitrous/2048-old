ba2825a9175d2415288d7eef07fc2341a6c78af0
Nathan Alo <n20gaming2000@gmail.com>
2019-03-12 23:17:39 +0800

    use keybind container for grid actions

---
+++ Block.Game/Objects/Managers/GridInputContainer.vb
@@ -0,0 +1,24 @@
+﻿Imports osu.Framework.Input.Bindings
+
+Namespace Objects.Managers
+    Public Class GridInputContainer : Inherits KeyBindingContainer(Of MoveDirection)
+
+        Public Overrides ReadOnly Property DefaultKeyBindings As IEnumerable(Of KeyBinding)
+            Get
+                Return New List(Of KeyBinding) From {
+                    New KeyBinding(New InputKey() {InputKey.Up}, MoveDirection.Up),
+                    New KeyBinding(New InputKey() {InputKey.Down}, MoveDirection.Down),
+                    New KeyBinding(New InputKey() {InputKey.Left}, MoveDirection.Left),
+                    New KeyBinding(New InputKey() {InputKey.Right}, MoveDirection.Right)
+                }
+            End Get
+        End Property
+    End Class
+
+    Public Enum MoveDirection
+        Up
+        Right
+        Down
+        Left
+    End Enum
+End Namespace
