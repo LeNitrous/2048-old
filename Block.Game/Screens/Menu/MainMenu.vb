cce83081a2c94a057ca65bc04f9520996e8b9f22
Nathan Alo <n20gaming2000@gmail.com>
2019-03-14 22:35:05 +0800

    completely rework main menu

--- Block.Game/Screens/Menu/MainMenu.vb
+++ Block.Game/Screens/Menu/MainMenu.vb
@@ -11,31 +11,61 @@ Imports Block.Game.Screens.Play
 
 Namespace Screens.Menu
     Public Class MainMenu : Inherits Screen
+        Private Selector As RuleSelector
+
         <Resolved>
         Private Property Stack As ScreenStack
 
         <BackgroundDependencyLoader>
         Private Sub Load(ByVal colour As BlockColour, ByVal audio As AudioManager, ByVal store As TextureStore)
-            Dim music = audio.Track.Get("presenterator")
-            music.RestartPoint = 24
-            music.Looping = True
-            music.Start()
+            Dim logo = New Sprite With {
+                .Anchor = Anchor.Centre,
+                .Origin = Anchor.Centre,
+                .Texture = store.Get("logo"),
+                .Colour = colour.FromHex("#776e65"),
+                .Scale = New Vector2(1.5),
+                .Y = -200
+            }
+
+            Selector = New RuleSelector With {
+                .Anchor = Anchor.Centre,
+                .Origin = Anchor.Centre,
+                .SelectRule = AddressOf OnPlay,
+                .Y = 50
+            }
 
             Content.AddRange(New List(Of Drawable) From {
-                New Sprite With {
+                logo,
+                Selector,
+                New MenuButton("multi") With {
                     .Anchor = Anchor.Centre,
                     .Origin = Anchor.Centre,
-                    .Texture = store.Get("logo"),
-                    .Colour = colour.FromHex("#776e65"),
-                    .Scale = New Vector2(1.5),
-                    .Y = -150
+                    .Y = 300
+                },
+                New MenuButton("exit") With {
+                    .Anchor = Anchor.Centre,
+                    .Origin = Anchor.Centre,
+                    .Position = New Vector2(-300, 180)
+                },
+                New MenuButton("leaderboard") With {
+                    .Anchor = Anchor.Centre,
+                    .Origin = Anchor.Centre,
+                    .Position = New Vector2(300, 180)
+                },
+                New MenuButton("settings") With {
+                    .Anchor = Anchor.TopRight,
+                    .Origin = Anchor.TopRight,
+                    .Scale = New Vector2(0.5),
+                    .Position = New Vector2(-10, 50)
                 }
             })
+
+            logo.MoveToY(-190, 1000, Easing.OutSine).Then().MoveToY(-200, 1000, Easing.OutSine).Loop()
         End Sub
 
-        Private Sub OnPlay(ByVal rule As IGameRule, Optional ByVal size As Integer = 4)
+        Private Sub OnPlay(Optional ByVal size As Integer = 4)
             Stack.Exit()
-            Stack.Push(New Player(rule, 4))
+            Stack.Push(New Player(Selector.GetSelectedRule(), 4))
         End Sub
     End Class
 End Namespace
--- Block.Game/Screens/Menu/MenuButton.vb
+++ Block.Game/Screens/Menu/MenuButton.vb
@@ -1,19 +1,20 @@
 ﻿Imports osu.Framework.Allocation
 Imports osu.Framework.Graphics
 Imports osu.Framework.Graphics.Containers
+Imports osu.Framework.Graphics.Shapes
 Imports osu.Framework.Graphics.Sprites
 Imports osu.Framework.Graphics.Textures
 Imports osu.Framework.Input.Events
 Imports osuTK
 Imports osuTK.Graphics
 Imports Block.Game.Graphics
-Imports Block.Game.Graphics.Shapes
 
 Namespace Screens.Menu
     Public Class MenuButton : Inherits ClickableContainer
         Public ReadOnly TextureIcon As String
         Public ReadOnly ClickAction As Action
-        Private Flash As RoundedBox
+        Protected Flash As Box
+        Protected Icon As Sprite
 
         Public Sub New(ByVal icon As String, Optional ByVal onClick As Action = Nothing)
             TextureIcon = icon
@@ -22,31 +23,28 @@ Namespace Screens.Menu
 
         <BackgroundDependencyLoader>
         Private Sub Load(ByVal store As TextureStore, ByVal colours As BlockColour)
-            Size = New Vector2(142)
+            Size = New Vector2(120)
+            Masking = True
             Rotation = 45.0F
-            EdgeEffect = New EdgeEffectParameters With {
-                .Colour = Color4.Black,
-                .Hollow = True,
-                .Radius = 5.0F,
-                .Type = EdgeEffectType.Shadow
-            }
-            Flash = New RoundedBox With {
+            CornerRadius = 20.0F
+            Flash = New Box With {
                 .RelativeSizeAxes = Axes.Both,
                 .Colour = Color4.WhiteSmoke,
                 .Alpha = 0
             }
+            Icon = New Sprite With {
+                .Anchor = Anchor.Centre,
+                .Origin = Anchor.Centre,
+                .Texture = store.Get("Interface/" & TextureIcon),
+                .Size = New Vector2(80),
+                .Rotation = -45.0F
+            }
             Children = New List(Of Drawable) From {
-                New RoundedBox With {
+                New Box With {
                     .RelativeSizeAxes = Axes.Both,
                     .Colour = colours.DarkerBrown
                 },
-                New Sprite With {
-                    .Anchor = Anchor.Centre,
-                    .Origin = Anchor.Centre,
-                    .Texture = store.Get("Interface/" & TextureIcon),
-                    .Size = New Vector2(80),
-                    .Rotation = -45.0F
-                },
+                Icon,
                 Flash
             }
         End Sub
@@ -55,7 +53,7 @@ Namespace Screens.Menu
             Flash.Alpha = 0.9F
             Flash.FadeOut(300, Easing.OutQuart)
             If Not ClickAction Is Nothing Then
-                ClickAction.Invoke
+                ClickAction.Invoke()
             End If
             Return MyBase.OnClick(e)
         End Function
